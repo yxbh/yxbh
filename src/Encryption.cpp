35c57d6e33245a1af61bc74d3bddfbe4d1140590
BenH <hhyyxx008@hotmail.com>
2013-02-03 12:41:31 +0800

    Encryption class update
    structural change to the class. It is now fully functional and appear to
    work correctly with shifting and unshifting. permutation and
    unpermutation is still work in progress.

--- src/Encryption.cpp
+++ src/Encryption.cpp
@@ -1,14 +1,60 @@
 #include "Encryption.h"
+#include <QDebug>
 
-Encryption::Encryption()
-      : m_key(0)
+Encryption::Encryption(unsigned short key, string & opseq, unsigned short charsiz)
+      : m_Key(key), m_OpSequence(opseq), m_CharSetSize(charsiz)
 {
+}
+
+string Encryption::encrypt(const string &str)
+{
+      int n = m_OpSequence.length();
+      qDebug() << "encrypt() string size = " << n << endl;
+      string buf = str;
+      for (int i  = 0; i <n; ++i)
+      {
+            switch(m_OpSequence[i])
+            {
+            case 'p':
+                  buf = permute(buf);
+                  break;
+            case 's':
+                  buf = shift(buf, m_Key);
+                  break;
+            default:
+                  throw exception();
+                  break;
+            }
+      }
+      return buf;
+}
+
+string Encryption::decrypt(const string &str)
+{
+      int n = m_OpSequence.length();
+      string buf = str;
+      for (int i  = 0; i <n; ++i)
+      {
+            switch(m_OpSequence[i])
+            {
+            case 'p':
+                  buf = unpermute(buf);
+                  break;
+            case 's':
+                  buf = unshift(buf);
+                  break;
+            default:
+                  throw exception();
+                  break;
+            }
+      }
+      return buf;
 }
 
 string Encryption::shift(const string & text, const unsigned key)
 {
-      m_key = key;
-      srand(m_key);
+      m_Key = key;
+      srand(m_Key);
       string m_buf = "";
       int size = text.length();
       for (int i = 0; i < size; i++)
@@ -18,9 +64,14 @@ string Encryption::shift(const string & text, const unsigned key)
       return m_buf;
 }
 
+QString Encryption::shift(const QString & text, const unsigned key)
+{
+      return QString(shift(text.toStdString(), key).c_str());
+}
+
 string Encryption::unshift(const string & text) const
 {
-      srand(m_key);
+      srand(m_Key);
       string m_buf = "";
       int size = text.length();
       for (int i = 0; i < size; i++)
@@ -31,3 +82,20 @@ string Encryption::unshift(const string & text) const
       }
       return m_buf;
 }
+
+QString Encryption::unshift(const QString & text) const
+{
+      return QString(unshift(text.toStdString()).c_str());
+}
+
+string Encryption::permute(const string &str)
+{
+      return str; // TODO : implementation
+}
+
+string Encryption::unpermute(const string & str)
+{
+      return str; // TODO : implementation
+}
+
+
--- src/Encryption.h
+++ src/Encryption.h
@@ -9,20 +9,52 @@
 #define ENCRYPTION_H
 #include <cstdlib>
 #include <string>
-#include <string>
 using std::string;
+#include <QString>
+#include <vector>
+using std::vector;
+#include <exception>
+using std::exception;
 
 class Encryption
 {
 private:
-      unsigned int m_key;
-     //mutable  string m_buf;
+      unsigned short m_Key;   // key to seed srand()
+      string m_OpSequence ;   // sequence of operation
+      unsigned short m_CharSetSize;
+      vector<int> m_Perm;
+
+      // shift each char via rand() after srand(key)
+      string shift(const string & text, const unsigned key);
+      QString shift(const QString & text, const unsigned key);
+      // opposite of shift(%)
+      string unshift(const string & text) const;
+      QString unshift(const QString & text) const;
+      string permute(const string & str);
+      string unpermute(const string & str);
 
 public:
-      Encryption();
+      Encryption(unsigned short key, string & opseq , unsigned short charsiz = 0);
+      Encryption(unsigned short key, QString & opseq, unsigned short charsiz = 0)
+            : m_Key(key), m_OpSequence(opseq.toStdString()), m_CharSetSize(charsiz)
+      {
+      }
 
-      string shift(const string & text, const unsigned key);
-      string unshift(const string & text) const;
+      // encrypt std::string
+      string encrypt(const string & str);
+      // encrypt QString. (converted to std::string first)
+      QString encrypt(const QString & str)
+      {
+            return QString(encrypt(str.toStdString()).c_str());
+      }
+      // decrypt std::string
+      string decrypt(const string & str);
+      // decrypt QString. (converted to std::string first)
+      QString decrypt(const QString &str)
+      {
+            return QString(decrypt(str.toStdString()).c_str());
+      }
+
 };
 
 #endif // ENCRYPTION_H
