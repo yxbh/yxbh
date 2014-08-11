72632de4e28a94119025865b809b8d58074a73f0
Benjamin <yxbh@users.noreply.github.com>
2014-08-11 09:55:42 +0800

    Update ScopeFunc.hpp
    make the template class variadic so it can accept arbitrary number of functors.

--- include/bh/ScopeFunc.hpp
+++ include/bh/ScopeFunc.hpp
@@ -4,28 +4,57 @@
 
 #include <utility>
 
-/**
- * \class ScopeFunc
- * A RAII class that takes a single functor object, executing the object's operator()
- * in the ScopeFunc object's destructor when the ScopeFunc object is destroyed.
-*/
-template<class Functor_T>
-class ScopeFunc
+namespace bh
+{
+
+namespace detail
+{
+
+template<typename ... T> class ScopeFuncImpl;
+template<> class ScopeFuncImpl<> {};
+template<typename First_T, typename ... Rest_T>
+class ScopeFuncImpl<First_T, Rest_T...>
+    : private ScopeFuncImpl<Rest_T...>
 {
 private:
-    Functor_T m_Functor;
+    using BaseType = ScopeFuncImpl<Rest_T...>;
+
+    First_T m_Functor;
 
 public:
-    ScopeFunc(Functor_T && p_rrFunctor) : m_Functor(p_rrFunctor) {}
-    ~ScopeFunc(void) { m_Functor(); }
+    ScopeFuncImpl(First_T && p_Functor, Rest_T && ... p_Functors)
+        : BaseType(std::forward<Rest_T>(p_Functors)...), m_Functor(p_Functor) {}
 
+    ~ScopeFuncImpl(void) { m_Functor(); }
+}; // ScopeFuncImpl
+
+} // detail ns
+
+/**
+ * \class ScopeFunc
+ * A RAII class that takes an arbitrary number of functor objects, executing the objects' operator()
+ * in the ScopeFunc object's destructor when the ScopeFunc object is destroyed.
+*/
+template<typename ... Functor_T>
+class ScopeFunc
+    : private detail::ScopeFuncImpl<Functor_T...>
+{
+private:
+    using BaseType = detail::ScopeFuncImpl<Functor_T...>;
+
+public:
+    ScopeFunc(Functor_T && ... p_Functors)
+        : BaseType(std::forward<Functor_T>(p_Functors)...) {}
 };
 
-template<class Functor_T>
-ScopeFunc<Functor_T> makeScopeFunc(Functor_T && p_rrFunctor)
+
+template<typename ... Functor_T>
+ScopeFunc<Functor_T...> makeScopeFunc(Functor_T && ... p_rrFunctor)
 {
-    return ScopeFunc<Functor_T>(std::forward<Functor_T>(p_rrFunctor));
+    return ScopeFunc<Functor_T...>(std::forward<Functor_T>(p_rrFunctor) ... );
 }
 
-#define MakeScopeFunc(x) auto MAKE_UNIQUE_NAME() = makeScopeFunc(x);
-#define BH_MakeScopeFunc(x) MakeScopeFunc(x);
+} // BH ns
+
+#define MakeScopeFunc(...) auto MAKE_UNIQUE_NAME() = makeScopeFunc(__VA_ARGS__);
+#define BH_MakeScopeFunc(...) MakeScopeFunc(__VA_ARGS__);
